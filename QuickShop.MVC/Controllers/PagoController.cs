3240308b9e6dd3b4a1f2ea33199f82c51bdaa645
Martin Quiroz <martinquiroz_18@hotmail.com>
2025-06-21 23:02:23 -0300

    Proceso pago (vista)

---
+++ QuickShop.MVC/Controllers/PagoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickShop.MVC.Controllers
+{
+    public class PagoController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult ProcesoPago()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuickShop.MVC/wwwroot/js/procesoPago.js
@@ -0,0 +1,16 @@
+﻿const radioButtons = document.querySelectorAll('input[name="FormaEntrega"]');
+const cards = document.querySelectorAll('.card');
+const envioElement = document.getElementById('precioEnvio');
+const totalElement = document.getElementById('total');
+const productoPrecio = parseInt(document.getElementById('precioProducto').innerText.replace(/\D/g, ''));
+
+radioButtons.forEach((radio, index) => {
+    radio.addEventListener('change', () => {
+        cards.forEach(card => card.classList.remove('selected'));
+        cards[index].classList.add('selected');
+
+        const envioPrecio = parseInt(cards[index].querySelector('.precioEntrega').dataset.precio);
+        envioElement.textContent = `$ ${envioPrecio.toLocaleString("es-AR")}`;
+        totalElement.textContent = `$ ${(productoPrecio + envioPrecio).toLocaleString("es-AR")}`;
+    });
+});
